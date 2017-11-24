d530d8682a3e31999add49638bee017a086c3590
Harrison Fung <harrison.hok.fung@gmail.com>
2017-11-24 16:23:47 -0500

    changed port number

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require("path");
 
 var app = express();
 var PORT = 3000;
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -15,7 +16,7 @@ app.get("/", function(req, res) {
 //     res.sendFile(path.join(__dirname, "../7evenseconds/views/index.html"));
 //   });
   
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("App listening on PORT " + PORT);
 });
 
