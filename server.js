7a95cb81c186ee20b5733a7116fd322cb0d944a5
Harrison Fung <harrison.hok.fung@gmail.com>
2017-11-24 16:09:33 -0500

    chanegs

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-
+var PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/index.html"));
+    res.sendFile(path.join(__dirname, "../7evenseconds/views/index.html"));
   });
 
 app.get("/index", function(req, res) {
     res.sendFile(path.join(__dirname, "../7evenseconds/views/index.html"));
   });
   
-app.listen(process.env.PORT, function() {
+app.listen(3000, function() {
   console.log("App listening on PORT " + PORT);
 });
 
