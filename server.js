cd1c022b7001f39d165752a17ae7b24b41004b5d
Harrison Fung <harrison.hok.fung@gmail.com>
2017-11-24 16:38:11 -0500

    added .

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../views/index.html"));
+    res.sendFile(path.join(__dirname, "./views/index.html"));
   });
 
 // app.get("/index", function(req, res) {
