5728355fd09cd152b162430f516387c7dd23dfc0
Harrison Fung <harrison.hok.fung@gmail.com>
2017-11-24 15:18:20 -0500

    Added a Procfile

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../7evenseconds/views/landing.html"));
+  });
+
+app.get("/index", function(req, res) {
+    res.sendFile(path.join(__dirname, "../7evenseconds/views/index.html"));
+  });
+  
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
