5a64b3e8d61157c816dd6c4c04fc4919c87c594c
Harrison Fung <harrison.hok.fung@gmail.com>
2017-11-24 15:42:34 -0500

    changed path file

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../7evenseconds/views/landing.html"));
+    res.sendFile(path.join(__dirname, "../7evenseconds/views/index.html"));
   });
 
 app.get("/index", function(req, res) {
