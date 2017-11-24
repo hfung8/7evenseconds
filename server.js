7bba75625be4ae74d9a63d0048f674fa5177a3be
Harrison Fung <harrison.hok.fung@gmail.com>
2017-11-24 16:18:45 -0500

    added procfile

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../7evenseconds/views/index.html"));
   });
 
-app.get("/index", function(req, res) {
-    res.sendFile(path.join(__dirname, "../7evenseconds/views/index.html"));
-  });
+// app.get("/index", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../7evenseconds/views/index.html"));
+//   });
   
 app.listen(3000, function() {
   console.log("App listening on PORT " + PORT);
