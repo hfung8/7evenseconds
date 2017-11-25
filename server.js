dec6e50c760d44c067db7d564ab3b42350769ed0
Harrison Fung <harrison.hok.fung@gmail.com>
2017-11-25 09:00:00 -0500

    resolved pathing issue to landing page

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static('public'));
+app.use(express.static('views/public'));
 
 app.get("/landing", function(req, res) {
     res.sendFile(path.join(__dirname, "./views/public/landing.html"));
